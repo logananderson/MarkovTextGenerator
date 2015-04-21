ecd6e2798034649d656a00834b401e0e9dabade0
logananderson <logan@side.cr>
2015-04-21 14:30:06 -0700

    Create initial Xcode Project

---
+++ MarkovTextGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MarkovTextGenerator
+//
+//  Created by Logan Anderson on 4/21/15.
+//  Copyright (c) 2015 loganonthemove.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
