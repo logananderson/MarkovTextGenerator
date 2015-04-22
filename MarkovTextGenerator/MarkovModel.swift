727fd5151292836b3e7699f00c54906792325ab9
logananderson <logan@side.cr>
2015-04-22 10:30:08 -0700

    Create basic Markov Chain Text Gen

---
+++ MarkovTextGenerator/MarkovModel.swift
@@ -0,0 +1,13 @@
+//
+//  MarkovModel.swift
+//  MarkovTextGenerator
+//
+//  Created by Logan Anderson on 4/21/15.
+//  Copyright (c) 2015 loganonthemove.com. All rights reserved.
+//
+
+import Cocoa
+
+class MarkovModel: NSObject {
+    
+}
